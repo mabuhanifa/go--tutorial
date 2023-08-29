ab2d8da9e7c1a53bd0a182d14f58c38b283aa909
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-29 23:59:05 +0600

    printF format

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	/*
 		fmt.Println("hello world")
+
 		//scape sequence new line
 		fmt.Println("1 \n2")
 
@@ -18,13 +19,25 @@ func main() {
 		fmt.Println("\"hello world\"")
 	*/
 
-	var name, country string
-	var age int
-	var cgpa float32
+	/*
+		name := "Shourov"
+		var country string = "Bangladesh"
+		var age int = 27
+		var cgpa float32 = 2.54
 
-	name = "Shourov"
-	country = "Bangladesh"
-	age = 27
-	cgpa = 2.54
-	fmt.Println("His name is", name, "from", country, "age", age, "with CGPA", cgpa)
+		fmt.Println("His name is", name, "from", country, "age", age, "with CGPA", cgpa)
+
+	*/
+
+	name := "Shourov"
+	var country string = "Bangladesh"
+	var age int = 27
+	var cgpa float32 = 2.54
+
+	// printF format
+	fmt.Printf("%v is my name \n", name)
+	fmt.Printf(". My age is %v \n", age)
+	fmt.Printf(". %v is my country \n", country)
+	fmt.Printf(". My CGPA is %v \n", cgpa)
+
 }
