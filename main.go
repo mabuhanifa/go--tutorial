bb2f87e6f422e5cd492a8186591e749a0b135a18
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-29 23:38:14 +0600

    variable type

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	//scape sequence new line
-	fmt.Println("1 \n2")
+	/*
+		fmt.Println("hello world")
+		//scape sequence new line
+		fmt.Println("1 \n2")
 
-	//scape sequence tab
-	fmt.Println("1 \t2")
+		//scape sequence tab
+		fmt.Println("1 \t2")
 
-	//scape sequence \\
-	fmt.Println("1 \\ 2")
+		//scape sequence \\
+		fmt.Println("1 \\ 2")
 
-	//scape sequence " "
-	fmt.Println("\"hello world\"")
+		//scape sequence " "
+		fmt.Println("\"hello world\"")
+	*/
 
+	var name, country string
+	var age int
+	var cgpa float32
+
+	name = "Shourov"
+	country = "Bangladesh"
+	age = 27
+	cgpa = 2.54
+	fmt.Println("His name is", name, "from", country, "age", age, "with CGPA", cgpa)
 }
