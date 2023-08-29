16423db4d5bad5e1a3a164fcba0ac04ca1dff4f0
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-29 23:31:15 +0600

    scape sequence

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	//scape sequence new line
+	fmt.Println("1 \n2")
+
+	//scape sequence tab
+	fmt.Println("1 \t2")
+
+	//scape sequence \\
+	fmt.Println("1 \\ 2")
+
+	//scape sequence " "
+	fmt.Println("\"hello world\"")
+
+}
